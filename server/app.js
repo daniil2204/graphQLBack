d6f8981faa61b43df7d0cfe7899d325ba180d73a
daniil2204 <dragindaniilmeizu@gmail.com>
2023-08-17 14:40:49 +0300

    deploy version

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const { addItemValidation, registerValidation, loginValidation, addReviewToItemV
 const app = express();
 const PORT = 5000  || process.env.PORT ;
 
-mongoose.connect('mongodb+srv://daniilfrilanc:Ddv22042004@cluster0.uewffss.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGODB)
 
 
 const permissions = shield({
@@ -44,6 +44,3 @@ DBconnected.once('open', () => console.log('DB is ok'))
 app.listen(PORT,(err) => {
     err ? console.log('Server error') : console.log('Server OK');
 });
-
-
-// { "Authorization": "Basic eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjRkMTJmN2U5ZWE3MzU5ZjcwMjMzNDI2IiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJpYXQiOjE2OTE0MzA3ODIsImV4cCI6MTY5NDAyMjc4Mn0.PNo_mCLVF5Bl5Gftmia-aXn2zSmx04jewwuUZuBCakg"}
